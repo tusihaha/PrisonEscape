81d9a9abd8f76bdcc6aa02a6a677c596ec5b26fe
Toan <toanleduc1@gmail.com>
2021-03-08 17:24:10 +0700

    init

---
+++ src/settings.4cfac.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/LupinCell.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"03765"}};
